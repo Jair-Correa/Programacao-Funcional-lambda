7b32287ee7e5e12484c21af9730b33a6bac9d47d
Jair Correa <jrabc1991@gmail.com>
2022-10-27 21:15:58 -0300

    funtion_predicate2

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,13 +5,14 @@ import java.util.List;
 import java.util.Locale;
 
 import entities.Product;
+import util.ProductPredicate;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
-		
+
 		List<Product> list = new ArrayList<>();
 
 		list.add(new Product("TV", 900.0));
@@ -21,7 +22,7 @@ public class Program {
 
 		// remover com função PREDICATE
 		/* Para todo p -> talq todo p seja igual ou maior a 100 */
-		list.removeIf(p -> p.getPrice() >= 100);
+		list.removeIf(new ProductPredicate());
 
 		/* pra cada produto p na minha lista list */
 		for (Product p : list) {
